1a1ee32e53394a4558afa53906af91506225022b
Marco Regueira <marcoregueira@gmail.com>
2018-04-27 14:18:37 +0200

    Nuget package

--- src/GenFu/Properties/AssemblyInfo.cs
+++ src/GenFu/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GenFu")]
+[assembly: AssemblyProduct("GenFu. Non official, thread safe, instantiable.")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
